9a093551b80c0e692873ee6a72c5ece02fe7ef42
Sshado <sshadout@gmail.com>
2019-01-17 22:52:46 -0500

    Inject logging and log handling
    Implement logging and an exception handler with PostSharp for sake of development time. These pieces have a high likelihood of being refactored later.

---
+++ Buttplug.Client.Platforms.Bluetooth/Aspects/LoggingExceptionsHandledAttribute.cs
@@ -0,0 +1,36 @@
+﻿#region Header
+// Buttplug.Client.Platforms.Bluetooth/ThrowLoggingExceptionHandled.cs - Created on 2019-01-17 at 10:41 PM by Sshado.
+// This file is part of Buttplug.io which is BSD licensed.
+#endregion
+
+using System ;
+
+using PostSharp.Aspects ;
+using PostSharp.Patterns.Diagnostics ;
+using PostSharp.Patterns.Diagnostics.Contexts ;
+
+namespace Buttplug.Client.Platforms.Bluetooth.Aspects
+{
+    /// <summary>
+    ///     Wraps a logging exception handler around <see cref="LoggingServices"/> implementations.
+    /// </summary>
+    internal class LoggingExceptionsHandledAttribute : OnMethodBoundaryAspect, ILoggingExceptionHandler
+    {
+        public void OnInternalException(LoggingExceptionInfo exceptionInfo)
+        {
+            throw new Exception("Internal logging failed with exception.", exceptionInfo.Exception);
+        }
+
+        public void OnInvalidUserCode(ref CallerInfo callerInfo, LoggingTypeSource source, string message, params object[] args)
+        {
+            throw new InvalidOperationException(string.Format(message, args));
+        }
+
+        public static void Initialize()
+        {
+            LoggingServices.ExceptionHandler = new ThrowLoggingExceptionHandler();
+        }
+
+        public override void OnEntry(MethodExecutionArgs args) => Initialize() ;
+    }
+}
