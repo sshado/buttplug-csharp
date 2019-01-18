bc69fbf15cd2cacbee3af15593b0dbebf1e20674
Sshado <sshadout@gmail.com>
2019-01-17 22:55:47 -0500

    Create new project: CrossPlatformConsole
    Create a new project to serve as development REPL and service implementation mockup.

---
+++ Buttplug.Examples.CrossPlatformConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Buttplug.Client.Platforms.Bluetooth ;
+
+namespace Buttplug.Examples.CrossPlatformConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var runtime = new BluetoothRuntime () ;
+            runtime.Entry();
+        }
+    }
+}
