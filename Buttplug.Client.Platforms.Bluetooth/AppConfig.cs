02cf187571ab975241ac86c7030e07a55ce7339d
Sshado <sshadout@gmail.com>
2019-01-18 22:29:11 -0500

    Add configuration support and MS.Extensions packages
    Add configuration support via AppConfig.cs and appsettings.json, as well as support for the wider Microsoft.Extensions namespace in preparation for new classes.

---
+++ Buttplug.Client.Platforms.Bluetooth/AppConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buttplug.Client.Platforms.Bluetooth
+{
+    public class AppConfig
+    {
+        public string Options { get ; set ; }
+    }
+}
