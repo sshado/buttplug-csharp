0cd0ecc8f4d69bae6cf05544dfbd4c08ee2ed0dd
Sshado <sshadout@gmail.com>
2019-01-20 02:03:57 -0500

    Expand RuntimeExtensions for Exceptions
    Add the ability to print out a list of inner exceptions with a simple join.

--- Buttplug.Client.Platforms.Bluetooth/Runtime/RuntimeExtensions.cs
+++ Buttplug.Client.Platforms.Bluetooth/Runtime/RuntimeExtensions.cs
@@ -20,11 +20,19 @@ namespace Buttplug.Client.Platforms.Bluetooth.Runtime
     [ EditorBrowsable ( EditorBrowsableState.Never ) ]
     public static class RuntimeExtensions
     {
-        public static IServiceCollection AddMicroservices ( this IServiceCollection services )
+        public static IServiceCollection AddMicroservices ( this IServiceCollection services, IEnumerable <IMicroService> micros )
         {
+            foreach ( IMicroService microservice in micros )
+            {
+                services.AddSingleton <IMicroService> ( microservice ) ;
+            }
 
-            //services.AddHostedService<>
-            throw new NotImplementedException () ;
+            return services ;
+        }
+
+        public static string ToDebugString<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            return "{" + string.Join(",", dictionary.Select(kv => kv.Key + "=" + kv.Value).ToArray()) + "}";
         }
     }
 }
