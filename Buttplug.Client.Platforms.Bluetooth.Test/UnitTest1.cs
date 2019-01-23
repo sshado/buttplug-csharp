225c054b525b5a418343db55fa8d6f98d9b63146
Sshado <sshadout@gmail.com>
2019-01-22 23:34:32 -0500

    Update ReadMe formatting and Logs

---
+++ Buttplug.Client.Platforms.Bluetooth.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Buttplug.Client.Platforms.Bluetooth.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
